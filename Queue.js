9d36f39a5d5295d6c9ff3cb125430b5517f235ef
MFAC <ninafroes@gmail.com>
2020-03-24 13:36:18 +0100

    Queue implementation

---
+++ Queue.js
@@ -0,0 +1,51 @@
+const Queue = function() {
+  const collection = [];
+
+  // Logs to the console the collection
+  this.print = function(){
+    console.log(collection);
+  }
+
+  // Adds an item to the back of the queue
+  this.enqueue = function(element){
+    collection.push(element);
+  }
+
+  // Removes an item from the front of the queue
+  this.dequeue = function(){
+    return collection.shift();
+  }
+
+  // Returns the first item of the Queue
+  this.front = function() {
+    return collection[0];
+  }
+
+  //Returns the size of the queue
+  this.size = function(){
+    return collection.length;
+  }
+
+  // Checks if the queue is empty
+  this.isEmpty = function(){
+    return (collection.length === 0);
+  }
+}
+
+const myQueue = new Queue();
+
+myQueue.enqueue(5);
+myQueue.enqueue(56);
+myQueue.enqueue(23);
+myQueue.enqueue(98);
+myQueue.print();
+myQueue.enqueue(4);
+myQueue.print();
+myQueue.dequeue();
+myQueue.print();
+console.log(myQueue.isEmpty());
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+console.log(myQueue.isEmpty());
