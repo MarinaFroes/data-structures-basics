b2cca8f965eba902fd7255470d1e430c915a7687
MFAC <ninafroes@gmail.com>
2021-01-08 16:43:04 +0100

    feat: Add reverse words

---
+++ interviewcake/reverseWords.js
@@ -0,0 +1,10 @@
+const message = ['c', 'a', 'k', 'e', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 's', 't', 'e', 'a', 'l']
+
+const reverseWords = arrayOfChar => {
+  const joined = arrayOfChar.join(' ')
+  console.log(joined)
+}
+
+reverseWords(reverseWords)
+
+console.log(message)
